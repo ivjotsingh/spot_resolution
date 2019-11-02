0f3c1c548ee3948388901926ef4b395782c8c455
ivjot singh <ivjot@supply.ai>
2019-11-03 02:51:04 +0530

    slot made array of slots in parking lot structure

--- main.go
+++ main.go
@@ -16,16 +16,41 @@ type slot struct {
 
 // embedding slots in parking lot
 type parkingLot struct {
-	slot              slot
+	slots             map[string]slot
 	totalNumberOfCars int
 }
 
+//func (p parkingLot) createParkingSlots()
+/*
+   def create_parking_slots(self, total_parking):
+       self.total_parking = total_parking
+
+       # OrderedDict is being used so that we can avoid expensive sorting operations whenever we want to find the
+       #  nearest empty slot available
+       slots = OrderedDict()
+       for x in range(total_parking):
+           slots[str(x+1)] = Slot(None, x + 1)
+       self.slots = slots
+       return f'Created a parking lot with {total_parking} slots'
+
+
+*/
 func main() {
 
 	car1 := car{"1234", "Red"}
+	car2 := car{"2345", "Green"}
 
 	slot1 := slot{car1, 1}
-	parkingLot1 := parkingLot{slot1, 1}
+	slot2 := slot{car2, 2}
+	parkingLot1 := new(parkingLot)
+	parkingLot1.totalNumberOfCars = 2
+	parkingLot1.slots = make(map[string]slot)
+	parkingLot1.slots["1"] = slot1
+	parkingLot1.slots["2"] = slot2
 
-	fmt.Println(parkingLot1.slot.car.color)
+	for no, slot := range parkingLot1.slots {
+		fmt.Println(no)
+		fmt.Println(slot.car.color)
+	}
+
 }
