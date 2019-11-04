af4e13e87ec16e5e03ff76e2dbe7ca316c9bd35f
ivjot singh <ivjot@supply.ai>
2019-11-04 16:27:11 +0530

    get nearest parking slot available function made

--- main.go
+++ main.go
@@ -1,27 +1,97 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-// defining a struct of car
-type car struct {
+// Car defining a struct of car
+type Car struct {
 	registerationNumber string
 	color               string
 }
 
-//embedding car with slot
-type slot struct {
-	car        car
+// Slot embedding car with slot
+type Slot struct {
+	car        Car
 	slotNumber int
 }
 
-// embedding slots in parking lot
-type parkingLot struct {
-	slots             map[string]slot
+// ParkingLot embedding slots in parking lot
+type ParkingLot struct {
+	slots             map[int]Slot
 	totalNumberOfCars int
 }
 
+func createParkingLot(totalNumberOfCars int) ParkingLot {
+
+	parking := new(ParkingLot)
+	parking.slots = make(map[int]Slot)
+
+	parking.totalNumberOfCars = totalNumberOfCars
+	for i := 1; i <= totalNumberOfCars; i++ {
+		parking.slots[i] = Slot{Car{}, i}
+	}
+	fmt.Println("created parking lot with slots: ", totalNumberOfCars)
+	return *parking
+
+}
+
+func (p ParkingLot) getNearestParkingSlot() int {
+	var keys []int
+	var emptySlot int
+	for k := range p.slots {
+		keys = append(keys, k)
+
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		if p.slots[k].car == (Car{}) {
+			emptySlot = k
+			break
+		}
+	}
+	return emptySlot
+}
+
+// func (p ParkingLot) getNearestParkingSlot() int (){
+// 	var keys []int
+// 	for k := range p.slots {
+//         keys = append(keys, k)
+// 	}
+// 	sort.Ints(keys)
+
+// 	return 1
+// }
+
+func main() {
+	totalNumberOfCars := 5
+	parking := createParkingLot(totalNumberOfCars)
+	nearestParkingSlot := parking.getNearestParkingSlot()
+	fmt.Println(nearestParkingSlot)
+}
+
+/*
 func main() {
 
+	totalNumberOfCars := 5
+	parking := createParkingLot(totalNumberOfCars)
+	// for no, slot := range parking.slots {
+	// 	if slot.car == (Car{}) {
+	// 		fmt.Println(slot.slotNumber)
+	// 		fmt.Println(no)
+	// 	}
+	// }
+	parking.getNearestParkingSlot()
+
+	/*
+		for _, slot := range parking.slots {
+			fmt.Println(slot.slotNumber)
+			fmt.Println(slot)
+		}
+*/
+
+/*
 	car1 := car{"1234", "Red"}
 	car2 := car{"2345", "Green"}
 
@@ -37,5 +107,6 @@ func main() {
 		fmt.Println(no)
 		fmt.Println(slot.car.color)
 	}
+*/
 
-}
+//}
