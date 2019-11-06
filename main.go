add3d9f57a0484b9bcc4f21c7ea71691a9c8ba81
ivjot singh <ivjot@supply.ai>
2019-11-07 01:57:40 +0530

    comments added

--- main.go
+++ main.go
@@ -23,32 +23,34 @@ type Slot struct {
 
 // ParkingLot embedding slots in parking lot
 type ParkingLot struct {
-	slots             map[int]Slot
-	totalNumberOfCars int
+	slots              map[int]Slot
+	totalNumberOfSlots int
 }
 
-func createParkingLot(totalNumberOfCars int) ParkingLot {
+// createParkingLot creates a parking lot object and assign slots to it
+func createParkingLot(totalNumberOfSlots int) ParkingLot {
 
 	parking := new(ParkingLot)
 	parking.slots = make(map[int]Slot)
 
-	parking.totalNumberOfCars = totalNumberOfCars
-	for i := 1; i <= totalNumberOfCars; i++ {
+	parking.totalNumberOfSlots = totalNumberOfSlots
+	for i := 1; i <= totalNumberOfSlots; i++ {
 		parking.slots[i] = Slot{make(map[int]Car), i}
 	}
-	fmt.Println("created parking lot with slots: ", totalNumberOfCars)
+	fmt.Println("created parking lot with slots: ", totalNumberOfSlots)
 	return *parking
 
 }
 
+// getNearestParkingSlot gets nearest empty slot
 func (parkingLot ParkingLot) getNearestParkingSlot() int {
 	var keys []int
 	var emptySlot int
 	for k := range parkingLot.slots {
 		keys = append(keys, k)
-
 	}
 	sort.Ints(keys)
+
 	for _, k := range keys {
 		if len(parkingLot.slots[k].car) == 0 {
 			emptySlot = k
@@ -58,17 +60,25 @@ func (parkingLot ParkingLot) getNearestParkingSlot() int {
 	return emptySlot
 }
 
-func (parkingLot ParkingLot) parkCar(car Car) {
+func (parkingLot ParkingLot) parkCar(car Car) bool {
 	nearestParkingSlot := parkingLot.getNearestParkingSlot()
+	if nearestParkingSlot == 0 {
+		fmt.Println("Parking is full")
+		return false
+	}
 	parkingLot.slots[nearestParkingSlot].car[nearestParkingSlot] = car
 	fmt.Println("Car", car.registerationNumber, "parked at:", nearestParkingSlot)
+	return true
 }
 
-func (parkingLot ParkingLot) freeSlot(slotNumber int) {
-	fmt.Println(parkingLot.slots)
-	fmt.Println(parkingLot.slots[slotNumber])
+func (parkingLot ParkingLot) freeSlot(slotNumber int) bool {
+	if len(parkingLot.slots[slotNumber].car) == 0 {
+		fmt.Println("Slot Number:", slotNumber, "is already free")
+		return false
+	}
 	parkingLot.slots[slotNumber] = Slot{make(map[int]Car), slotNumber}
-	fmt.Println(parkingLot.slots[slotNumber])
+	fmt.Println("Slot Number:", slotNumber, "is free")
+	return true
 }
 
 func (parkingLot ParkingLot) searchCarByColor(color string) []int {
@@ -78,20 +88,21 @@ func (parkingLot ParkingLot) searchCarByColor(color string) []int {
 			parkingSlotsWithCars = append(parkingSlotsWithCars, k)
 		}
 	}
-	fmt.Println(parkingSlotsWithCars)
+	fmt.Println("slot numbers of cars with color", color, parkingSlotsWithCars)
 	return parkingSlotsWithCars
 
 }
 
-func (parkingLot ParkingLot) searchCarByRegisterationNumber(registerationNumber string) int {
-	var parkingSlotWithCar int
-	for _, v := range parkingLot.slots {
+func (parkingLot ParkingLot) searchCarByRegisterationNumber(registerationNumber string) []int {
+	parkingSlotsWithCars := []int{}
+	for k, v := range parkingLot.slots {
 		if v.car[v.slotNumber].registerationNumber == registerationNumber {
-			parkingSlotWithCar = v.slotNumber
+			parkingSlotsWithCars = append(parkingSlotsWithCars, k)
+
 		}
 	}
-	fmt.Println(parkingSlotWithCar)
-	return parkingSlotWithCar
+	fmt.Println("slot numbers of cars with Registeration Number", registerationNumber, parkingSlotsWithCars)
+	return parkingSlotsWithCars
 }
 
 func (parkingLot ParkingLot) getParkingLotStatus() {
@@ -110,11 +121,13 @@ func main() {
 	parkingLot := createParkingLot(totalNumberOfSlots)
 
 	for {
-		fmt.Println("\n Available commands are \n 1: park registrationNumber color \n 2: leave slotNumber \n 3: status\n 4: slotNumbersForCarsWithColour color \n 5: slotNumberForCarsWithRegistrationNumber registrationNumber)
+		fmt.Println("\n Available commands are \n 1: park registrationNumber color \n 2: leave slotNumber \n 3: status\n 4: slotNumbersForCarsWithColour color \n 5: slotNumberForCarsWithRegistrationNumber registrationNumber")
 		reader := bufio.NewReader(os.Stdin)
 		inputCommand, _ := reader.ReadString('\n')
+		inputCommand = strings.TrimSuffix(inputCommand, "\n")
 
 		command := strings.Split(inputCommand, " ")
+		fmt.Println(command[0])
 
 		switch command[0] {
 		case "park":
@@ -122,7 +135,9 @@ func main() {
 			color := command[2]
 			parkingLot.parkCar(Car{registerationNumber: registrationNumber, color: color})
 		case "leave":
-			slotNumber, _ := strconv.Atoi(command[1])
+			slotNumber, err := strconv.Atoi(command[1])
+			fmt.Println(err)
+			fmt.Println(slotNumber)
 			parkingLot.freeSlot(slotNumber)
 		case "status":
 			parkingLot.getParkingLotStatus()
@@ -131,10 +146,14 @@ func main() {
 		case "slotNumberForCarsWithRegistrationNumber":
 			parkingLot.searchCarByRegisterationNumber(command[1])
 		default:
-			fmt.Println("Wrong command, Available commands are \n 1: park registrationNumber color \n 2: leave slotNumber \n 3: status\n 4: slotNumbersForCarsWithColour color \n 5: slotNumberForCarsWithRegistrationNumber registrationNumber")
+			fmt.Println("Wrong command")
 		}
 	}
 
+	// parkingLot.parkCar(Car{registerationNumber: "1234", color: "Green"})
+	// parkingLot.parkCar(Car{registerationNumber: "1274", color: "Green"})
+	// parkingLot.freeSlot(1)
+	// parkingLot.parkCar(Car{registerationNumber: "5345", color: "Green"})
 	// parkingLot.searchCarByColor("Green")
 	// parkingLot.searchCarByRegisterationNumber("5345")
 	// parkingLot.getParkingLotStatus()
