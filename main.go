38b47bcf4e86e1f355259c07caaabe1f6db73614
ivjot singh <ivjot@supply.ai>
2019-11-07 00:29:28 +0530

    switch case added

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 // Car defining a struct of car
@@ -57,10 +61,11 @@ func (parkingLot ParkingLot) getNearestParkingSlot() int {
 func (parkingLot ParkingLot) parkCar(car Car) {
 	nearestParkingSlot := parkingLot.getNearestParkingSlot()
 	parkingLot.slots[nearestParkingSlot].car[nearestParkingSlot] = car
-
+	fmt.Println("Car", car.registerationNumber, "parked at:", nearestParkingSlot)
 }
 
 func (parkingLot ParkingLot) freeSlot(slotNumber int) {
+	fmt.Println(parkingLot.slots)
 	fmt.Println(parkingLot.slots[slotNumber])
 	parkingLot.slots[slotNumber] = Slot{make(map[int]Car), slotNumber}
 	fmt.Println(parkingLot.slots[slotNumber])
@@ -98,14 +103,39 @@ func (parkingLot ParkingLot) getParkingLotStatus() {
 }
 
 func main() {
-	totalNumberOfSlots := 5
+
+	fmt.Println("Enter the no of slots in parking lot")
+	var totalNumberOfSlots int
+	fmt.Scanf("%d", &totalNumberOfSlots)
 	parkingLot := createParkingLot(totalNumberOfSlots)
-	parkingLot.parkCar(Car{registerationNumber: "1234", color: "Red"})
-	parkingLot.parkCar(Car{registerationNumber: "2345", color: "Green"})
-	parkingLot.freeSlot(1)
-	parkingLot.parkCar(Car{registerationNumber: "5345", color: "Green"})
-	fmt.Println(parkingLot)
-	parkingLot.searchCarByColor("Green")
-	parkingLot.searchCarByRegisterationNumber("5345")
-	parkingLot.getParkingLotStatus()
+
+	for {
+		fmt.Println("\n Available commands are \n 1: park registrationNumber color \n 2: leave slotNumber \n 3: status\n 4: slotNumbersForCarsWithColour color \n 5: slotNumberForCarsWithRegistrationNumber registrationNumber)
+		reader := bufio.NewReader(os.Stdin)
+		inputCommand, _ := reader.ReadString('\n')
+
+		command := strings.Split(inputCommand, " ")
+
+		switch command[0] {
+		case "park":
+			registrationNumber := command[1]
+			color := command[2]
+			parkingLot.parkCar(Car{registerationNumber: registrationNumber, color: color})
+		case "leave":
+			slotNumber, _ := strconv.Atoi(command[1])
+			parkingLot.freeSlot(slotNumber)
+		case "status":
+			parkingLot.getParkingLotStatus()
+		case "slotNumbersForCarsWithColour":
+			parkingLot.searchCarByColor(command[1])
+		case "slotNumberForCarsWithRegistrationNumber":
+			parkingLot.searchCarByRegisterationNumber(command[1])
+		default:
+			fmt.Println("Wrong command, Available commands are \n 1: park registrationNumber color \n 2: leave slotNumber \n 3: status\n 4: slotNumbersForCarsWithColour color \n 5: slotNumberForCarsWithRegistrationNumber registrationNumber")
+		}
+	}
+
+	// parkingLot.searchCarByColor("Green")
+	// parkingLot.searchCarByRegisterationNumber("5345")
+	// parkingLot.getParkingLotStatus()
 }
